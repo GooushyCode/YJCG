d7d4d3858096344bb6d77cfe79d0e28b06aa4d99
gooushy <gooushy@163.com>
2022-07-03 16:54:56 +0800

    1-4 优化启动日志

---
+++ management/src/main/java/com/yijia/management/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.yijia.management.mapper;
+
+import com.yijia.management.vo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    int login(String username,String password);
+    User findAll();
+}
---
+++ management/src/main/java/com/yijia/management/service/UserService.java
@@ -0,0 +1,25 @@
+package com.yijia.management.service;
+import com.yijia.management.mapper.UserMapper;
+import com.yijia.management.vo.User;
+import org.springframework.stereotype.Service;
+import javax.annotation.Resource;
+
+@Service
+public class UserService {
+    @Resource
+    private UserMapper userMapper;
+
+    public int login(String username,String password){
+        int sum = userMapper.login(username, password);
+        if (sum==1){
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+
+    public User findAll(){
+        return userMapper.findAll();
+
+    }
+}
