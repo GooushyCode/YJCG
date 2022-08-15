03dabb5dcb66379fd3dd6023837b45c2061479ad
gooushy <gooushy@163.com>
2022-08-15 18:20:29 +0800

    1-4 优化启动日志

---
+++ management/src/main/java/com/yijia/management/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.yijia.management.controller;
+import com.yijia.management.service.UserService;
+import com.yijia.management.vo.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
+
+@RestController
+public class TestController {
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "hello";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @RequestMapping("/login")
+    public int login(String username,String password){
+        return userService.login(username, password);
+    }
+
+    @GetMapping("findAll")
+    public String findAll(){
+        User user = userService.findAll();
+        return user.getUsername();
+    }
+
+}
